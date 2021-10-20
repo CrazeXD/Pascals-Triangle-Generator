b6e2733a4c326d430dc14f6339087b0193b27c94
Craze <crayzmayz100@gmail.com>
2021-10-19 23:07:26 -0700

    Completed equation generator

--- pascalstriangle.py
+++ pascalstriangle.py
@@ -1,5 +1,5 @@
 def generate_triangle(exponent, triangle=[]):
-  for row in range(exponent):
+  for row in range(exponent+1):
     currentrow = []
     currentrow.append(1)
     if row == 0:
@@ -7,10 +7,13 @@ def generate_triangle(exponent, triangle=[]):
     elif row == 1:
       currentrow.append(1)
     else:
-      for i in triangle[row-1]:
+      prevrow = triangle[row-1]
+      count = 0
+      for j in prevrow:
+        count+=1
+      for i in range(1, count):
         val = triangle[row-1][i-1]+triangle[row-1][i]
         currentrow.append(val)
       currentrow.append(1)
     triangle.append(currentrow)
   return triangle
-triangle = []
